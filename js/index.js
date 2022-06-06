dd939eaafa97f35d7ba45574d9421096cf06b108
Emiliano Scanga <scangaemiliano@gmail.com>
2022-06-06 12:12:19 +0200

    Change: increase number system for Lottery Card

--- js/index.js
+++ js/index.js
@@ -1,26 +1,38 @@
 
 // Funzione per aumentare i numeri nella card section-prizes
+let lotteryValueAmount = 130043;
 
-function changeNumberPromise(){
-    return new Promise((resolve, reject) =>{
+/* Funzione che si occupa di convertire la variabile lotteryValueAmount in string, aggiunge la
+   virgola e poi effettua il display nel sito
+*/
+function displayCardValues(value){
+    const lotteryValue = document.getElementById("lotteryValue");
+    value = String(value);
+    value = value.split(" ",3);
+
+    lotteryValue.textContent = value;
+}
+
+// Funzione che si occuppa di aggiungere un range random da aumentare a lotteryValueAmount
+
+async function changeCardValues(){
+    let amountToChange = Math.round((Math.random() + 1) * 10);
+
+    for(let i = 0; i < amountToChange; i++){
+        let numberChanger = await promiseChangeCardValue();
+        displayCardValues(numberChanger);
+    }
+}
+
+function promiseChangeCardValue(){
+    return new Promise((resolve) =>{
         setTimeout(() =>{
-            numberToChangeValue = Number(numberToChangeValue.replace(",",""));
-            numberToChangeValue += 1;
-            numberToChangeValue = String(numberToChangeValue);
-            numberToChangeValue = numberToChangeValue.slice(0,3) + "," + numberToChangeValue.slice(3);
-            resolve(numberToChangeValue);
+            lotteryValueAmount += 1;
+            resolve(lotteryValueAmount);
         },100);
     })
 }
 
-async function changeNumber(){
-    const numberToChange = document.querySelector(".numberChanger");
-    numberToChangeValue = numberToChange.textContent;
-    let amountToChange = Math.round((Math.random() + 1) * 10);
-    for(let i = 0; i < amountToChange; i++){
-        let numberChanger = await changeNumberPromise();
-        numberToChange.textContent = numberChanger;
-    }
-}
+setInterval(changeCardValues,1000);
+window.addEventListener("DOMContentLoaded", displayCardValues(lotteryValueAmount));
 
-setInterval(changeNumber,30000);
