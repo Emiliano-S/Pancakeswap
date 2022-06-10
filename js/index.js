dd91560b56bf291b353fc8ad0ce3361c01aa1028
Emiliano G. Scanga <scangaemiliano@gmail.com>
2022-06-10 21:25:44 +0200

    End: Newest Collections

---
+++ js/index.js
@@ -0,0 +1,6 @@
+import {arrayCoins} from './modules/arrayCoinsNFTs.js';
+import {createCoinCard} from './modules/coinsCardCreatorNFTs.js';
+
+arrayCoins.forEach(coin => {
+    createCoinCard(coin);
+});
---
+++ js/modules/arrayCoinsNFTs.js
@@ -0,0 +1,51 @@
+export const arrayCoins =
+[
+    {
+        name: "Baked Potatoes",
+        id: "bakedPotatoesCard",
+        value: "11,967",
+        imgProfile: "./img/NFTs/collections/baked-avatar.png",
+        imgBackground: "./img/NFTs/collections/baked-banner.webp",
+        link: "https://pancakeswap.finance/nfts/collections/0x4D89eBf7b1F66806A4C99Df71D22b7b9efC6bB0b"
+    },
+    {
+        name: "Horror Ape Club",
+        id: "horrorApeClubCard",
+        value: "52,3116",
+        imgProfile: "./img/NFTs/collections/horror-ape-club-avatar.png",
+        imgBackground: "./img/NFTs/collections/horror-ape-club-banner.webp",
+        link: "https://pancakeswap.finance/nfts/collections/0xAFc7647b584730694B9606511F11F423A0816eFf"
+    },
+    {
+        name: "Bitpunks",
+        id: "bitpunksCard",
+        value: "9,85",
+        imgProfile: "./img/NFTs/collections/bitpunks-avatar.png",
+        imgBackground: "./img/NFTs/collections/bitpunks-banner.webp",
+        link: "https://pancakeswap.finance/nfts/collections/0x2513743c6ffB7868F31087C2ad3aFd91bb90BA3C"
+    },
+    {
+        name: "PixelSweeper",
+        id: "pixelSweeperCard",
+        value: "976,7083",
+        imgProfile: "./img/NFTs/collections/pixel-sweeper-avatar.png",
+        imgBackground: "./img/NFTs/collections/pixel-sweeper-banner.webp",
+        link: "https://pancakeswap.finance/nfts/collections/0x5F41842CFF838120271d772C6994F051d418a4aD"
+    },
+    {
+        name: "Degen Ape Club",
+        id: "degenApeClubCard",
+        value: "854,58924",
+        imgProfile: "./img/NFTs/collections/degen-ape-club-avatar.png",
+        imgBackground: "./img/NFTs/collections/degen-ape-club-banner.webp",
+        link: "https://pancakeswap.finance/nfts/collections/0x8e311dB45B55DF68b7C1C8D01888a4C43986c60F"
+    },
+    {
+        name: "Liquid Monsters",
+        id: "liuidMonstersCard",
+        value: "152,88153",
+        imgProfile: "./img/NFTs/collections/liquid-monsters-avatar.png",
+        imgBackground: "./img/NFTs/collections/liquid-monsters-banner.webp",
+        link: "https://pancakeswap.finance/nfts/collections/0x0D464bDde2301C30871bB4C29bB7DD935f5a985C"
+    }
+]
