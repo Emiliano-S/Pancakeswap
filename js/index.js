31af348412cf1694f734259a897aad24ce9ad992
Emiliano G. Scanga <scangaemiliano@gmail.com>
2022-06-11 17:12:23 +0200

    End: FAQs NTFs

--- js/index.js
+++ js/index.js
@@ -9,3 +9,9 @@ arrayCoinsPresentation.forEach(presentation => {
 arrayCoinsCard.forEach(coin =>{
     createCoinCard(coin, "nftsNewestArrivals");
 });
+
+document.getElementById("faqsContent").addEventListener("click",(event)=>{
+    if(event.target.classList == "container-faqs-question"){
+        event.target.nextElementSibling.classList.toggle("visible");
+    }
+});
