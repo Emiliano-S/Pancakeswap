5fd030d3d537c7e538cc4517355296c40163b241
Emiliano G. Scanga <scangaemiliano@gmail.com>
2022-07-11 16:46:17 +0200

    Fix: animation

--- js/index.js
+++ js/index.js
@@ -32,12 +32,14 @@ countdown();
 let slideIndex = 0;
 showSlides();
 
+let timer = setTimeout(showSlides, 5000);
+
 function showSlides() {
   let i;
   let slides = document.getElementsByClassName("banner-container");
 //   let dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+    slides[i].classList.add("hide");
   }
   slideIndex++;
   if (slideIndex > slides.length) {
@@ -46,10 +48,35 @@ function showSlides() {
 //   for (i = 0; i < dots.length; i++) {
 //     dots[i].className = dots[i].className.replace(" active", "");
 //   }
-  slides[slideIndex - 1].style.display = "block";
+  slides[slideIndex - 1].classList.remove("hide");
 //   dots[slideIndex - 1].className += " active";
-  setTimeout(showSlides, 5000);
 }
 
 
+document.getElementById('btnBannerR').addEventListener('click', ()=>{
+    const banner02 = document.getElementById('banner02');
+    const banner01 = document.getElementById('banner01');
 
+    if(banner02.classList.contains('hide')){
+      banner02.classList.remove('hide');
+      banner01.classList.add('hide');
+      clearTimeout(timer);
+      setInterval(()=>{
+        timer = setTimeout(showSlides, 5000);
+      }, 5000)
+    }
+})
+
+document.getElementById('btnBannerL').addEventListener('click', ()=>{
+  const banner02 = document.getElementById('banner02');
+  const banner01 = document.getElementById('banner01');
+
+  if(banner01.classList.contains('hide')){
+    banner01.classList.remove('hide');
+    banner02.classList.add('hide');
+    clearTimeout(timer);
+    setInterval(()=>{
+      timer = setTimeout(showSlides, 5000);
+    }, 5000)
+  }
+})
